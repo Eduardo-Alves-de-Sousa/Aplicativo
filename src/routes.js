5756de950c60a522f877ea04ee5585f15031cac9
Eduardo-Alves-de-Sousa <55249802+Eduardo-Alves-de-Sousa@users.noreply.github.com>
2021-03-04 17:16:46 -0300

    Atualizaçoes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express"; //importr apenas o Routers do express
 
+import User from "./app/models/User";
+
 const routes = new Router();
 
 routes.get("/", (req, res) => {
   return res.json({ mensagem: "helo world" });
+});
+
+routes.get("/user", async (req, res) => {
+  const user = await User.create({
+    name: "Eduardo Alves",
+    email: "eduardo@uespi.br",
+    password_hash: "123456",
+  });
+  res.json(user);
 });
 
 export default routes;
