cde43e9a24e449aad540727cef4d7878614cb8d2
Eduardo-Alves-de-Sousa <55249802+Eduardo-Alves-de-Sousa@users.noreply.github.com>
2021-03-05 00:14:17 -0300

    Atualizaçoes

---
+++ src/app/controllers/UserControler.js
@@ -0,0 +1,45 @@
+import User from "../models/User";
+
+class userController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ mensagem: "Usuario já existe" });
+    }
+
+    const { id, name, email, prestador_servico } = await User.create(req.body);
+
+    return res.json({ id, name, email, prestador_servico });
+  }
+  async update(req,res){
+    const {email, passwordAntigo } = req.body;
+    const user = await User.findByPk(req.userId) //buscar o usuario no banco de dados pelo id
+
+    //verificar se ele quer alterar o email, comparar com o email atual
+    if(email != user.email){
+      const existeUsuarioEmail = await User.findOne({where: {email}});
+      if(existeUsuarioEmail){//usuario já existe
+        return res.status(400).json({error: "Email já cadastrado"});
+      }
+    }
+
+    //verificar se a senha antiga é a senha cadastrada no banco
+    //só vai realizar a verificação se ele informou a senha antiga
+    console.log(passwordAntigo)
+    if(!passwordAntigo){
+      return res.status(400).json({error: 'Senha antiga não informada'});
+    }
+
+    if(!(await user.checkPassword(passwordAntigo))){
+      return res.status(400).json({error: 'Senha antiga diferente da senha cadastrada'});
+    }
+
+    const { id, name, prestador_servico } = await user.update(req.body);
+
+    return res.json({ id, name, email, prestador_servico });
+  }
+
+}
+
+export default new userController();
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,25 @@
 import { Router } from "express"; //importr apenas o Routers do express
 
+import UserController from "./app/controllers/UserControler";
+
+import SessionControler from "./app/controllers/SessionControler"
+
+import AuthMiddlware from './app/Middlewares/auth'
+
 import User from "./app/models/User";
 
 const routes = new Router();
 
+routes.post("/sessions", SessionControler.store);
+
+routes.post("/user", UserController.store);
+
+routes.use(AuthMiddlware);
+//todas as rotas após o middlwares só serão executadas se o ususario estiver logado
+routes.put('/user', UserController.update)
+
 routes.get("/", (req, res) => {
   return res.json({ mensagem: "helo world" });
-});
-
-routes.get("/user", async (req, res) => {
-  const user = await User.create({
-    name: "Eduardo Alves",
-    email: "eduardo@uespi.br",
-    password_hash: "123456",
-  });
-  res.json(user);
 });
 
 export default routes;
